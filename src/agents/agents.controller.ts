eea0161d42f4fa946595b8457e8aa0cc45b74c68
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-28 09:07:23 -0500

    [ADD] controller and services to create - agents

--- src/agents/agents.controller.ts
+++ src/agents/agents.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, Body, HttpStatus } from '@nestjs/common';
+
+import { CreateAgentDTO } from './dto/create-agent.dto'
+import { AgentsService } from './agents.service'
 
 @Controller('agents')
-export class AgentsController {}
+export class AgentsController {
+
+  constructor(private agentsService:AgentsService) {}
+
+  @Post('/create')
+  async createAgent(@Res() res, @Body() createAgentDTO: CreateAgentDTO) {
+    const agent = await this.agentsService.createAgent(createAgentDTO)
+    return res.status(HttpStatus.CREATED).json({
+      message: 'Agent Successfully Created',
+      agent
+    })
+  }
+
+}
--- src/agents/agents.service.ts
+++ src/agents/agents.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { CreateAgentDTO } from './dto/create-agent.dto'
+import { Agent } from './interfaces/agent.interface'
 
 @Injectable()
-export class AgentsService {}
+export class AgentsService {
+
+  constructor(@InjectModel('Agent') private agentModel: Model<Agent>) {}
+
+  async createAgent(createAgentDTO: CreateAgentDTO) : Promise<Agent> {
+    const agent = new this.agentModel(createAgentDTO)
+    return agent
+  }
+
+}
