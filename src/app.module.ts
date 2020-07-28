1f901daa066fdd7e255b62fafa86a3977a02f78c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-28 06:54:24 -0500

    [ADD] controller, service, module - problems

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgentsModule } from './agents/agents.module';
 import { UsersModule } from './users/users.module';
+import { ProblemsModule } from './problems/problems.module';
 
 @Module({
-  imports: [AgentsModule, UsersModule],
+  imports: [AgentsModule, UsersModule, ProblemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
