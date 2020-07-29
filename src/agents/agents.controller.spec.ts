c61c71fd9238cea2d7658fd17f60dc502506f99c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-28 23:48:01 -0500

    [ADD] test to agents

--- src/agents/agents.controller.spec.ts
+++ src/agents/agents.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AgentsController } from './agents.controller';
+import { BadRequestException } from '@nestjs/common';
+import { AgentsService } from './agents.service';
 
 describe('Agents Controller', () => {
   let controller: AgentsController;
+  let service: AgentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AgentsController],
+      providers: [AgentsService],
     }).compile();
 
+    service = module.get<AgentsService>(AgentsService);
     controller = module.get<AgentsController>(AgentsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  describe('create a agent', () => {
+    it('throws an error when no data is provided', async () => {      
+      const name = 'Andres'
+      const availability = false
+
+      expect.assertions(2);
+
+      try {
+        await service.createAgent({ name, availability })
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException);
+        expect(e.message).toBe('Id is required.');
+      }
+    });
+
+    it('should create a product', async () => {
+      const user = {
+        name:'Andres',
+      }
+      jest.spyOn(service, 'createAgent').mockImplementation(() => name)
+    })
+  })
 });
